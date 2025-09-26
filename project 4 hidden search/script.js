66e7210c529d687b0d197c75ef340d7804202597
hirodinn <hirebikila1@gmail.com>
2025-09-26 13:02:51 +0300

    done project 4

---
+++ project 4 hidden search/script.js
@@ -0,0 +1,4 @@
+document.querySelector("button").addEventListener("click", () => {
+  document.querySelector(".container").classList.toggle("hidden");
+  document.querySelector("input").focus();
+});
