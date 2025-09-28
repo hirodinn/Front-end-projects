7fdf08e31c5160a5682115ebc2495ade3551c92d
hirodinn <hirebikila1@gmail.com>
2025-09-28 22:33:39 +0300

    add the drawing functionality by circle around the mouse point and make the input and buttons interactive

---
+++ project 22 Drawing App/script.js
@@ -0,0 +1,46 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const clearButton = document.getElementById("clear");
+const increaseButton = document.getElementById("increase");
+const decreaseButton = document.getElementById("decrease");
+const input = document.getElementById("color");
+
+clearButton.addEventListener("click", () => {
+  ctx.clearRect(0, 0, 800, 800); // clears a part
+});
+
+let size = 20;
+let color = "black";
+
+increaseButton.addEventListener("click", () => {
+  if (size === 50) return;
+  size += 5;
+  updateSize();
+});
+decreaseButton.addEventListener("click", () => {
+  if (size === 5) return;
+  size -= 5;
+  updateSize();
+});
+input.addEventListener("input", (e) => {
+  color = e.target.value;
+});
+function updateSize() {
+  document.getElementById("size").innerText = size;
+}
+
+function drawCircle(x, y) {
+  ctx.beginPath();
+  ctx.arc(x, y, size, 0, Math.PI * 2);
+  ctx.fillStyle = color;
+  ctx.fill();
+}
+canvas.addEventListener("mousedown", (e) => {
+  canvas.addEventListener("mousemove", paint);
+});
+canvas.addEventListener("mouseup", (e) => {
+  canvas.removeEventListener("mousemove", paint);
+});
+function paint(e) {
+  drawCircle(e.offsetX, e.offsetY);
+}
