142ab915ff59636f97f6220d81b60353933a7729
hirodinn <hirebikila1@gmail.com>
2025-09-30 16:14:16 +0300

    done script.js

---
+++ project 16 Drink water/script.js
@@ -0,0 +1,30 @@
+const options = document.querySelectorAll(".option");
+options.forEach((option, i) => {
+  option.addEventListener("click", () => {
+    selectOptionsTill(i);
+  });
+});
+function selectOptionsTill(n) {
+  if (
+    options[n].classList.contains("selected") &&
+    !options[n].nextElementSibling.classList.contains("selected")
+  ) {
+    n--;
+  }
+  options.forEach((option, i) => {
+    if (i <= n) option.classList.add("selected");
+    else option.classList.remove("selected");
+  });
+  changeHeight(n + 1);
+}
+
+function changeHeight(n) {
+  const filledHeight = 12.5 * n;
+  const unfilledHeight = 12.5 * (8 - n);
+  document.querySelector(".unfilled").style.height = `${unfilledHeight}%`;
+  document.querySelector(".unfilled h3").innerText = `${
+    (2 * unfilledHeight) / 100
+  }L`;
+  document.querySelector(".filled").style.height = `${filledHeight}%`;
+  document.querySelector(".filled h3").innerText = `${filledHeight}%`;
+}
