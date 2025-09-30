074d1020691c24d8231c31a7aad9ebbd52a0ce61
hirodinn <hirebikila1@gmail.com>
2025-09-30 16:56:55 +0300

    finish the script.js which also has the image slider functionality

--- project 26 vertical slider/script.js
+++ project 26 vertical slider/script.js
@@ -1,16 +1,19 @@
 const up = document.querySelector(".up");
 const down = document.querySelector(".down");
 const infos = document.querySelectorAll(".info");
+const images = document.querySelectorAll(".image");
 let currentIndex = 0;
 up.addEventListener("click", () => {
   if (currentIndex == 3) currentIndex = 0;
   else currentIndex++;
   updateInfo();
+  updateImage();
 });
 down.addEventListener("click", () => {
   if (currentIndex == 0) currentIndex = 3;
   else currentIndex--;
   updateInfo();
+  updateImage();
 });
 function updateInfo() {
   infos.forEach((info, i) => {
@@ -20,4 +23,13 @@ function updateInfo() {
     console.log(info.style.top);
   });
 }
+function updateImage() {
+  images.forEach((image, i) => {
+    if (i < currentIndex) image.style.bottom = `${(currentIndex - i) * 100}vh`;
+    else if (i == currentIndex) image.style.bottom = "0";
+    else image.style.bottom = `-${(i - currentIndex) * 100}vh`;
+    console.log(image.style.bottom);
+  });
+}
 updateInfo();
+updateImage();
