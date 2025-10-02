19282e7244c0f0e01092f47d5886885bce91a5e6
hirodinn <hirebikila1@gmail.com>
2025-10-02 21:36:35 +0300

    add initial load and date display

--- project 19 Theme Clock/script.js
+++ project 19 Theme Clock/script.js
@@ -24,4 +24,29 @@ setInterval(() => {
     hour >= 12 ? "PM" : "AM"
   }`;
 }, 1000);
+function initialLoad() {
+  const now = new Date();
+  const hour = now.getHours();
+  const minute = now.getMinutes();
+  const second = now.getSeconds();
+
+  secondContainer.style.transform = `rotate(${second * 6 - 90}deg)`;
+  minuteContainer.style.transform = `rotate(${minute * 6 - 90}deg)`;
+  hourContainer.style.transform = `rotate(${(hour + minute / 60) * 30 - 90}deg)`;
+
+  timeContainer.innerText = `${hour} : ${minute < 10 ? "0" + minute : minute} ${
+    hour >= 12 ? "PM" : "AM"
+  }`;
+
+  const formatted = now.toLocaleDateString("en-US", {
+    weekday: "long",
+    month: "long",
+  });
+  const day = now.getDate();
+  date.innerHTML = `${formatted} <span>${day}</span>`;
+
+  console.log(now.toString());
+}
+
+initialLoad();
 
