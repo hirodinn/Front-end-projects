1af18db60dd0f53572a77c4f2cfce8cc6a9ee978
hirodinn <hirebikila1@gmail.com>
2025-10-02 10:40:55 +0300

    modify the script.js and style.css file so that the one heart doesn't interfere with the other

--- project 29 double click heart/script.js
+++ project 29 double click heart/script.js
@@ -1,29 +1,40 @@
 const image = document.querySelector(".image-container");
 const text = document.querySelector("h4 span");
-const heart = document.querySelector(".heart");
+
 let like = 0;
 let clicked = false;
 let key;
-let element = document.createElement("div");
-element.classList.add("heart");
-element.innerText = "❤️";
+
 image.addEventListener("click", (e) => {
   if (clicked) {
+    // Double click
     const x = e.offsetX;
     const y = e.offsetY;
+
     like++;
     text.innerText = like;
-    element.style.top = `${y}px`;
-    element.style.left = `${x}px`;
-    image.appendChild(element);
+
+    // create a NEW heart each time
+    const heart = document.createElement("div");
+    heart.classList.add("heart");
+    heart.innerText = "❤️";
+
+    heart.style.top = `${y}px`;
+    heart.style.left = `${x}px`;
+
+    image.appendChild(heart);
+
+    // remove heart after animation
+    setTimeout(() => heart.remove(), 1000);
+
     clicked = false;
     if (key) clearTimeout(key);
   } else {
+    // single click
     clicked = true;
-    if (image.querySelector(".heart")) image.removeChild(element);
-    console.log("single click");
     key = setTimeout(() => {
       clicked = false;
     }, 800);
   }
 });
+
