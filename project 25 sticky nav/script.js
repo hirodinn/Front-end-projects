f601541e6c238d767a1ef839888999a98e573273
hirodinn <hirebikila1@gmail.com>
2025-10-04 21:52:42 +0300

    found another way to do the scroll animation

--- project 25 sticky nav/script.js
+++ project 25 sticky nav/script.js
@@ -1,6 +1,16 @@
 const imageDiv = document.querySelector(".image-div");
 const nav = document.querySelector(".nav");
+// window.addEventListener("scroll", () => {
+//   if (window.scrollY > nav.offsetHeight + 150) nav.classList.add("triggered");
+//   else nav.classList.remove("triggered");
+// });
 window.addEventListener("scroll", () => {
-  if (window.scrollY > nav.offsetHeight + 150) nav.classList.add("triggered");
+  const triggerBottom = (window.innerHeight / 8) * 5;
+  console.log(
+    triggerBottom,
+    document.querySelector("main").getBoundingClientRect().top
+  );
+  if (triggerBottom > imageDiv.getBoundingClientRect().bottom)
+    nav.classList.add("triggered");
   else nav.classList.remove("triggered");
 });
