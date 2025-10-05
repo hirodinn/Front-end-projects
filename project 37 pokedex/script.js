b8b1fb90ce37fb91678d0e19514012e476c565ec
hirodinn <hirebikila1@gmail.com>
2025-10-05 14:33:45 +0300

    done the script.js of the pokemon to load all cards at the same time

---
+++ project 37 pokedex/script.js
@@ -0,0 +1,54 @@
+const typeColors = {
+  fire: "#FDDFDF",
+  grass: "#DEFDE0",
+  electric: "#FCF7DE",
+  water: "#DEF3FD",
+  ground: "#f4e7da",
+  rock: "#d5d5d4",
+  fairy: "#fceaff",
+  poison: "#98d7a5",
+  bug: "#f8d5a3",
+  dragon: "#97b3e6",
+  psychic: "#eaeda1",
+  flying: "#F5F5F5",
+  fighting: "#E6E0D4",
+  normal: "#F5F5F5",
+};
+
+async function getPokemon(id) {
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+  return res.json();
+}
+
+async function load() {
+  const container = document.querySelector(".container");
+
+  const promises = [];
+  for (let i = 1; i <= 150; i++) {
+    promises.push(getPokemon(i));
+  }
+
+  const allPokemon = await Promise.all(promises);
+
+  allPokemon.forEach((data) => {
+    const type = data.types[0].type.name;
+    const color = typeColors[type] || "#777";
+
+    const el = document.createElement("div");
+    el.classList.add("card");
+    el.style.backgroundColor = color;
+
+    el.innerHTML = `
+            <div class="image-container">
+              <img src="${data.sprites.front_default}" alt="${data.name}">
+            </div>
+            <div class="id">#${data.id.toString().padStart(3, "0")}</div>
+            <div class="name">${data.name}</div>
+            <div class="type">${type}</div>
+          `;
+
+    container.appendChild(el);
+  });
+}
+
+load();
