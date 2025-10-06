1b42a5e8d1ce9df643a84631ea950e696ce86860
hirodinn <hirebikila1@gmail.com>
2025-10-06 18:58:07 +0300

    implement password generation logic with randomized quotas

--- project 31 password generator/script.js
+++ project 31 password generator/script.js
@@ -10,4 +10,52 @@ const symbols = ["/", "?", "<", ">", "{", "}", "|", "@", "!", "&"];
 
 const generateButton = document.querySelector(".generate-password");
 const passwordText = document.querySelector(".password-text");
+generateButton.addEventListener("click", () => {
+  const upperCaseCheck = document.querySelector(".uppercase").checked;
+  const lowerCaseCheck = document.querySelector(".lowercase").checked;
+  const numbersCheck = document.querySelector(".number").checked;
+  const symbolsCheck = document.querySelector(".symbols").checked;
+
+  let range = [];
+  if (upperCaseCheck) range.push(upperCaseLetters);
+  if (lowerCaseCheck) range.push(lowerCaseLetters);
+  if (numbersCheck) range.push(numbers);
+  if (symbolsCheck) range.push(symbols);
+  if (!range.length) return;
+
+  const len = document.querySelector(".length input").value;
+  let password = "";
+  let quotas = assignQuota(range.length, len);
+
+  for (let i = 0; i < len; i++) {
+    let current = returnRandom(0, range.length);
+    password += range[current][returnRandom(0, range[current].length)];
+    quotas[current]--;
+    if (quotas[current] === 0) {
+      quotas.splice(current, 1);
+      range.splice(current, 1);
+    }
+  }
+
+  passwordText.innerText = password;
+});
+
+function returnRandom(start, end) {
+  return start + Math.floor(Math.random() * (end - start));
+}
+
+function assignQuota(n, total) {
+  let arr = [];
+  for (let i = 0; i < n; i++) {
+    arr.push(1);
+  }
+  total -= n;
+  for (let i = 0; i < n && total > 0; i++) {
+    let rand = returnRandom(0, total + 1);
+    arr[i] += rand;
+    total -= rand;
+  }
+  arr[arr.length - 1] += total;
+  return arr;
+}
 
