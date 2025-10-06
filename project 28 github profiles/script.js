846a890744e08e0b4de51eaf3afe752cb1cc367a
hirodinn <hirebikila1@gmail.com>
2025-10-06 13:40:30 +0300

    implement GitHub user data fetch with error handling

--- project 28 github profiles/script.js
+++ project 28 github profiles/script.js
@@ -7,4 +7,20 @@ input.addEventListener("keyup", (e) => {
     console.log("runned");
   }
 });
+async function loadRepository(username) {
+  const element = document.querySelector(".response");
+  element.innerHTML = "";
+
+  const response = await fetch(`https://api.github.com/users/${username}`, {
+    headers: { Accept: "application/vnd.github+json" },
+  });
+
+  if (!response.ok) {
+    console.error("Error:", response.status, response.statusText);
+    element.innerHTML =
+      "<h1 style='margin:auto;text-align:center;'>No User</h1>";
+    return;
+  }
+
+  const res = await response.json();
 
