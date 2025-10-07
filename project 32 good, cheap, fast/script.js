d2f46f322873727a634f67d7a776247a9b116962
hirodinn <hirebikila1@gmail.com>
2025-10-07 14:24:14 +0300

    Implement slider display logic and kill function

--- project 32 good, cheap, fast/script.js
+++ project 32 good, cheap, fast/script.js
@@ -10,5 +10,29 @@ sliderContainer.forEach((slider, i) => {
       displayResult(i);
     }
   });
-});
+});function displayResult(i) {
+  let alive = 0;
+  for (let j = 0; j < live.length; j++) {
+    if (i === j) continue;
+    if (live[j]) alive++;
+  }
+  if (alive === 0 || alive === 1) {
+    sliderContainer[i].querySelector(".slide").classList.add("move");
+    sliderContainer[i].classList.add("change-color");
+  } else {
+    let removeIndex;
+    if (i === 1) removeIndex = 2;
+    else if (i === 0) removeIndex = 1;
+    else removeIndex = 0;
+    sliderContainer[i].querySelector(".slide").classList.add("move");
+    sliderContainer[i].classList.add("change-color");
+    kill(removeIndex);
+  }
+}
+
+function kill(i) {
+  sliderContainer[i].querySelector(".slide").classList.remove("move");
+  sliderContainer[i].classList.remove("change-color");
+  live[i] = false;
+}
 
