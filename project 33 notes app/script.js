01a36f737017456cb19c85cb644f36767376dd62
hirodinn <hirebikila1@gmail.com>
2025-10-07 14:30:09 +0300

    add edit and delete event handlers for notes

--- project 33 notes app/script.js
+++ project 33 notes app/script.js
@@ -12,3 +12,36 @@ addToCart.addEventListener("click", () => {
   }
 });
 
+function addEventForEdit() {
+  const editButton = document.querySelectorAll(".edit");
+  editButton.forEach((edit, i) => {
+    edit.addEventListener("click", () => {
+      if (current) {
+        note[i] = document.querySelector("textarea").value;
+        localStorage.setItem("note", JSON.stringify(note));
+        loadNotes();
+      } else {
+        let text = edit.parentNode.parentNode.querySelector(".note").innerText;
+        edit.parentNode.parentNode.querySelector(
+          ".note"
+        ).innerHTML = `<textarea rows='20'>${text}</textarea>`;
+        const textarea = document.querySelector("textarea");
+        textarea.focus();
+        textarea.selectionStart = textarea.selectionEnd = textarea.value.length;
+        current = true;
+      }
+    });
+  });
+}
+
+function addEventForDelete() {
+  const editButton = document.querySelectorAll(".delete");
+  editButton.forEach((del, i) => {
+    del.addEventListener("click", () => {
+      note.splice(i, 1);
+      localStorage.setItem("note", JSON.stringify(note));
+      loadNotes();
+    });
+  });
+}
+
