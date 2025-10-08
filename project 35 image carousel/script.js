21a396bb113eb5ea8038cdc568072429d0f9bf67
hirodinn <hirebikila1@gmail.com>
2025-10-08 22:08:43 +0300

    add previous and next button controls to the image slider

--- project 35 image carousel/script.js
+++ project 35 image carousel/script.js
@@ -12,3 +12,22 @@ function setInt() {
 
 setInt();
 
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+
+prev.addEventListener("click", () => {
+  clearInterval(key);
+  current--;
+  if (current === -1) current = 8;
+  imageContainer.style.left = `-${current * 500}px`;
+  setInt();
+});
+
+next.addEventListener("click", () => {
+  clearInterval(key);
+  current++;
+  if (current === 9) current = 0;
+  imageContainer.style.left = `-${current * 500}px`;
+  setInt();
+});
+
