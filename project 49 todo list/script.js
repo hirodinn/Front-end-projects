97d91fffb4cf6199f25f97721f29194333372fd5
hirodinn <hirebikila1@gmail.com>
2025-10-12 01:13:29 +0300

    render todos and add toggle/delete functionality with mouse events

--- project 49 todo list/script.js
+++ project 49 todo list/script.js
@@ -12,3 +12,39 @@ input.addEventListener("keyup", (e) => {
   }
 });
 
+function loadTodo() {
+  const todo = JSON.parse(localStorage.getItem("todo"));
+  todoContainer.innerHTML = "";
+  todo.forEach((t) => {
+    const el = document.createElement("div");
+    el.innerText = t;
+    todoContainer.appendChild(el);
+  });
+  addEvent();
+}
+
+function addEvent() {
+  const todos = todoContainer.querySelectorAll("div");
+
+  todos.forEach((todo) => {
+    todo.addEventListener("contextmenu", (event) => {
+      event.preventDefault();
+    });
+  });
+
+  todos.forEach((todo) => {
+    todo.addEventListener("mouseup", (event, i) => {
+      if (event.button === 0) {
+        todo.classList.toggle("spoil");
+      } else if (event.button === 2) {
+        const t = JSON.parse(localStorage.getItem("todo"));
+        t.splice(i, 1);
+        localStorage.setItem("todo", JSON.stringify(t));
+        loadTodo();
+      }
+    });
+  });
+}
+
+loadTodo();
+
