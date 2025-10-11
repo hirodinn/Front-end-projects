0c4f41a1760a15b4e44b1dc38cf3bdb30b0a53e4
hirodinn <hirebikila1@gmail.com>
2025-10-11 17:51:32 +0300

    Add generatequestion class and sample questions

---
+++ project 46 quiz app/script.js
@@ -0,0 +1,41 @@
+class GenerateQuestion {
+  constructor(question, choices, answer) {
+    this.question = question;
+    this.choices = choices;
+    this.answer = answer;
+  }
+}
+
+const questions = [
+  new GenerateQuestion(
+    "Which language runs in a web browser?",
+    ["Java", "C", "Pyhon", "JavaScript"],
+    "3"
+  ),
+  new GenerateQuestion(
+    "What does CSS stand for?",
+    [
+      "Central Style Sheets",
+      "Cascading Style Sheets",
+      "Cascading Simple Sheets",
+      "Cars SUVs Sailboats",
+    ],
+    "1"
+  ),
+  new GenerateQuestion(
+    "What does HTML stand for?",
+    [
+      "Hypertext Markup Language",
+      "Hypertext Markdown Language",
+      "Hyperloop Machine Language",
+      "Helicopters Terminals Motorboats Lamborginis",
+    ],
+    "0"
+  ),
+  new GenerateQuestion(
+    "What year was Javascript launched?",
+    [1996, 1995, 1994, "none of the above"],
+    "1"
+  ),
+];
+
