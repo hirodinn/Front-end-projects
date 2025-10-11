8a0c060a52cf7dc13c8b8b74a01b744ff19aa547
hirodinn <hirebikila1@gmail.com>
2025-10-11 17:53:38 +0300

    implement question rendering and answer handling logic

--- project 46 quiz app/script.js
+++ project 46 quiz app/script.js
@@ -43,4 +43,49 @@ let current = 0;
 let score = 0;
 const container = document.querySelector(".container");
 
+function choosed() {
+  const radios = document.querySelectorAll("input");
+  let check;
+  radios.forEach((radio) => {
+    if (radio.checked) check = radio;
+  });
+  return check;
+}
+
+function loadContainer() {
+  container.innerHTML = `<h2>${questions[current].question}</h2>
+      <form>
+        <input type="radio" name="choice" value="0" />
+        <label>${questions[current].choices[0]}</label>
+        <br />
+        <input type="radio" name="choice" value="1" />
+        <label>${questions[current].choices[1]}</label>
+        <br />
+        <input type="radio" name="choice" value="2" />
+        <label>${questions[current].choices[2]}</label>
+        <br />
+        <input type="radio" name="choice" value="3" />
+        <label>${questions[current].choices[3]}</label>
+      </form>
+      <button>Submit</button>`;
+  loadEventListenerButton();
+}
+
+function loadEventListenerButton() {
+  const button = document.querySelector("button");
+  button.addEventListener("click", () => {
+    const answered = choosed();
+    console.log(answered.value);
+    if (answered) {
+      if (answered.value === questions[current].answer) {
+        score++;
+        console.log("correct");
+      }
+      current++;
+      if (current === 4) loadFinal();
+      else loadContainer();
+    }
+  });
+}
+
 
