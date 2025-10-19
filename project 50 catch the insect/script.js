122dd13d765412016f3ba4fa0bcedb5503ca365d
hirodinn <hirebikila1@gmail.com>
2025-10-19 23:16:02 +0300

    implement insect select and transition to the gameplay screen

--- project 50 catch the insect/script.js
+++ project 50 catch the insect/script.js
@@ -4,3 +4,18 @@ firstPageButton.addEventListener("click", () => {
   document.body.style.transform = "translateY(-100vh)";
 });
 
+// Second page Script
+let startTime;
+let insectChoice;
+document.querySelectorAll(".choose-insect-btn").forEach((insect) => {
+  insect.addEventListener("click", () => {
+    insectChoice = insect.querySelector("img").src;
+    console.log(insectChoice);
+    document.body.style.transform = "translateY(-200vh)";
+    start();
+    startTime = Date.now();
+    addInsect();
+    addEventForInsects();
+  });
+});
+
