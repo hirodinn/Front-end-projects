f8ad7d9534b14e4afef48bd80749c68cac7e835a
hirodinn <hirebikila1@gmail.com>
2025-10-19 23:17:12 +0300

    add game timer to track elapsed time during gameplay

--- project 50 catch the insect/script.js
+++ project 50 catch the insect/script.js
@@ -19,3 +19,25 @@ document.querySelectorAll(".choose-insect-btn").forEach((insect) => {
   });
 });
 
+// Third page Script
+const gamePage = document.querySelector(".third-page");
+const timeElement = document.querySelector(".timer");
+let score = 0;
+
+function startTimer() {
+  let elapsed = 0;
+  const now = Date.now();
+  const diff = now - startTime + elapsed; // total time
+  const minutes = Math.floor(diff / 60000);
+  const seconds = Math.floor((diff % 60000) / 1000);
+
+  // format mm:ss
+  timeElement.textContent = `${String(minutes).padStart(2, "0")}:${String(
+    seconds
+  ).padStart(2, "0")}`;
+}
+
+function start() {
+  setInterval(startTimer, 1000);
+}
+
