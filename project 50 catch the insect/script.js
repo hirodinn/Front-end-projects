5e3ddaf0ec64bd086c9430c3bdf0e62b226dddc6
hirodinn <hirebikila1@gmail.com>
2025-10-19 23:15:16 +0300

    add first page transition to move to insect selection screen

---
+++ project 50 catch the insect/script.js
@@ -0,0 +1,6 @@
+// First Page Script
+const firstPageButton = document.querySelector(".play");
+firstPageButton.addEventListener("click", () => {
+  document.body.style.transform = "translateY(-100vh)";
+});
+
